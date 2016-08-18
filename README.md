10f354ff2783acc4a9af18bd244bb5f15606821f
Oleksandr Viazlo (MEDUSA) <oleksandr.viazlo@hep.lu.se>
2016-08-18 21:03:05 +0200

    input arguments are passed through environment variables

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SmallScripts
+script use three environment variables:
+FILE - full name of input file
+HIST - name of hist in the inpput root file
+OUT - name of output file
+
+example of usage:
+FILE=/home/oviazlo/PhD_study/Wprime/CrossCheckWithMagnar_aug2016/files/Magnar/aug2016/mt_inputs_nosys.root  HIST=data OUT=magnar_data_mt_new.txt root -l getBinCounts.C
--- getBinCounts.C
+++ getBinCounts.C
@@ -1,33 +1,48 @@
-void getBinCounts(string fileName, string inHistName){
+void getBinCounts(){
 
-    string inFileName = fileName;
-    if (fileName==""){
-      cout << "[ERROR]\t Specify input file name!!!" << endl;
-      cout << "[ERROR]\t TERMINATE" << endl;
-      return; 
-    }
+  char* tmpCharArr = NULL;
+  tmpCharArr = getenv("FILE");
+  if (tmpCharArr==NULL){
+    cout << "[ERROR]\t Specify input file name!!!" << endl;
+    cout << "Setup environment FILE" << endl;
+    cout << "[ERROR]\t TERMINATE" << endl;
+    return; 
+  }
+  string inFileName(tmpCharArr);
+  
+  tmpCharArr = getenv("HIST");
+  if (tmpCharArr==NULL){
+    cout << "[ERROR]\t Specify hist name!!!" << endl;
+    cout << "Setup environment HIST" << endl;
+    cout << "[ERROR]\t TERMINATE" << endl;
+    return; 
+  }
+  string inHistName(tmpCharArr);
+  
+  tmpCharArr = getenv("OUT");
+  if (tmpCharArr==NULL){
+    cout << "[ERROR]\t Specify hist name!!!" << endl;
+    cout << "Setup environment OUT" << endl;
+    cout << "[ERROR]\t TERMINATE" << endl;
+    return; 
+  }
+  string outFileName(tmpCharArr);
+  
+  TFile* inFile = new TFile(inFileName.c_str(),"READ");
+  if (inFile->IsOpen()==false){
+    cout << "File <" << inFileName << "> s not found! TERMINATE!" << endl;
+    return;
+  }
+  
+  cout << "\nBinflow of hist: " << inHistName << endl;
+  TH1D* tmpHist = (TH1D*)inFile->Get(inHistName.c_str());
+  unsigned int nBins = tmpHist->GetNbinsX();
     
-    if (inHistName==""){
-      cout << "[ERROR]\t Specify hist name!!!" << endl;
-      cout << "[ERROR]\t TERMINATE" << endl;
-      return; 
-    }
-
-	TFile* inFile = new TFile(inFileName.c_str(),"READ");
-	const int nHists = 1;
-
-	string histName[nHists] =  {inHistName};
-
-	for (int i=0; i<nHists; i++){
-		cout << "\nBinflow of hist: " << histName[i] << endl;
-		TH1D* tmpHist = (TH1D*)inFile->Get(histName[i].c_str());
-		int nBins = tmpHist->GetNbinsX();
-		
-		for (int iBin=1; iBin<=nBins; iBin++){
-			cout << iBin << ":\t" << tmpHist->GetBinContent(iBin) << endl;
-		}
-	}
-
-
+  std::ofstream outFile;
+  outFile.open (outFileName.c_str(), std::ofstream::out | std::ofstream::trunc);
+  for (int iBin=1; iBin<=nBins; iBin++){
+    outFile << iBin << ":\t" << tmpHist->GetBinContent(iBin) << endl;
+  }
+  outFile.close();
 
 }
